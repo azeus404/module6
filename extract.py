326b264b3704c8d552f732e3a614203df776b281
azeus404 <59473482+azeus404@users.noreply.github.com>
2020-01-03 12:09:13 +0100

    Initial commit

---
+++ extract.py
@@ -0,0 +1,26 @@
+import argparse
+import tldextract
+import ipaddress
+
+"""
+Extract domains (LLD) dataset
+tokenization
+https://www.endgame.com/blog/technical-blog/using-deep-learning-detect-dgas
+v1
+"""
+
+parser = argparse.ArgumentParser(description='Process domainlist')
+parser.add_argument('path', help='an integer for the accumulator')
+
+
+args = parser.parse_args()
+path = args.path
+
+file = open(path,'r')
+d = file.readlines()
+for i in d:
+    e = tldextract.extract(i)
+    #print(e.domain)
+    if e.subdomain:
+        print(e.subdomain)
+    #print(e.fqdn)
