62feba372868d147e47f185b4746119467b150c4
“Leon <leon.wiskie@wiskieit.nl>
2020-01-21 13:45:36 +0100

    Better data splitting

--- combinedata.py
+++ combinedata.py
@@ -6,8 +6,15 @@ dnscat_df = pd.read_csv("./TRAININGS_DATA/lld_lab_dnscat_features_added.csv")
 iodine_df = pd.read_csv("./TEST_DATA/lld_lab_iodine_features_added.csv")
 
 
-vertical_stack = pd.concat([dnscat_df , iodine_df], axis=0)
+tunnel_df = pd.concat([dnscat_df , iodine_df], axis=0)
 
-print(vertical_stack.head)
+print(tunnel_df.head)
+print("Shuffel data")
+tunnel_df.sample(frac=1).reset_index(drop=True)
 
-vertical_stack.to_csv('lld_lab_tunnel_features_added.csv', index=False)
+print("Split data")
+test = tunnel_df.sample(frac=0.1,random_state=200)
+train = tunnel_df.drop(test.index)
+print("Write data..")
+train.to_csv('./TRAININGS_DATA/lld_lab_tunnel_features_added_1.csv', index=False)
+test.to_csv('./TEST_DATA/lld_lab_tunnel_features_added_1.csv', index=False)
